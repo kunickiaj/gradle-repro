1fc53ff1f07681751ca7c2927840c6fc77f2237d
Adam Kunicki <adam@streamsets.com>
2019-03-26 21:26:19 -0700

    initial commit.

---
+++ intermediateProject/leafProject1/src/main/java/com/adamkunicki/LeafMain.java
@@ -0,0 +1,11 @@
+package com.adamkunicki;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+public class LeafMain {
+  public static void main(String[] args) {
+    Map<String, String> map = ImmutableMap.of("hello", "world");
+  }
+}
